1d1c8c21b8399a8315382d0122bfb9bcc69687e1
kosh <kosh@rbtsb.com>
2014-08-21 23:05:50 +0800

    added a guide in the main layout

--- gen/com/styleme/floating/example/R.java
+++ gen/com/styleme/floating/example/R.java
@@ -25,9 +25,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int action_settings=0x7f080003;
         public static final int start=0x7f080000;
         public static final int stop=0x7f080001;
+        public static final int textView1=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -38,6 +39,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
+        public static final int guide=0x7f050005;
         public static final int hello_world=0x7f050001;
         public static final int start=0x7f050003;
         public static final int stop=0x7f050004;
